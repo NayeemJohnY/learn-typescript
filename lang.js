754a5ed00ed41e12239664e54d942ba7ede02416
NayeemJohnY <njnayeem@gmail.com>
2025-06-28 18:14:08 +0530

    Typescript learn

---
+++ lang.js
@@ -0,0 +1,18 @@
+
+function getInfo() {
+    if (firstName.length > 10){
+        console.log("PASS")
+    } else {
+        console.log("FAIL");
+        
+    }
+}
+
+getInfo();
+
+const user = {
+    firstName : "Tom",
+    age : 30
+}
+
+console.log(user.city); 
---
+++ name.ts
@@ -0,0 +1,29 @@
+console.log("Hello Typescript")
+//.ts -> tsc TS compiler  -> .js -> javascript run env (node, browser)
+
+
+// function getInfoTest() {
+//     if (firstName.length > 10){ // compile time error : Cannot find name 'firstName
+//         console.log("PASS")
+//     } else {
+//         console.log("FAIL");
+        
+//     }
+// }
+
+// getInfoTest();
+
+
+// const user = {
+//     firstName : "Tom",
+//     age : 30
+// }
+
+// console.log(user.city); // Property 'city' does not exist on type
+
+
+
+let age : number = 30;
+let fName: string = "Tom";
+let isActive: boolean = true;
+
